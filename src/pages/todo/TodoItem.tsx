1792cd9ab852260fe4c91fe93b96c7471b6a9d26
olegbucolo <olegbucolo97@gmail.com>
2025-12-31 16:14:36 +0100

    Create TodoItem as item in TodoData.json

---
+++ src/pages/todo/TodoItem.tsx
@@ -0,0 +1,25 @@
+import type { Todo } from '../../types/todo';
+import dayjs from 'dayjs';
+import './TodoItem.css';
+
+interface TodoItemProps {
+    todo: Todo;
+}
+
+export function TodoItem({ todo }: TodoItemProps) {
+    return (
+        <li className="todo-li">
+            <p className="todo-text">{todo.content}</p>
+            <div className="todo-actions">
+                <button>Modify</button>
+                <button>Done</button>
+            </div>
+            <div className="todo-details">
+                <span>id: {todo.id}</span>
+                
+                <span>date: {dayjs(todo.date).format('MMMM D')}</span>
+                <span>difficulty {todo.difficulty} </span>
+            </div>
+        </li>
+    )
+}
--- src/pages/todo/TodoList.tsx
+++ src/pages/todo/TodoList.tsx
@@ -1,16 +1,18 @@
 import './TodoList.css'
-import {useState} from 'react';
-import type {Todo} from '../../types/todo'
+import { useState } from 'react';
+import type { Todo } from '../../types/todo'
 import rawTodoData from '../../data/TodoData.json'
-
+import { TodoItem } from './TodoItem';
 
 export function TodoList() {
     const [todos, setTodos] = useState<Todo[]>(rawTodoData as Todo[])
-    todos.map(todo => console.log(todo))
-    console.log('EEEEEEEEEEEEEEEEEEEEEND')
     return (
         <section className="todo-list">
-
+            <ul>
+                {todos.map((todo) => {
+                    return <TodoItem todo={todo} />
+                })}
+            </ul>
         </section>
     )
 }
