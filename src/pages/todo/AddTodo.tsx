2549e4a17d73f253044fd6b64c4f832571408909
olegbucolo <olegbucolo97@gmail.com>
2026-01-03 12:08:00 +0100

    Add localStorage as persistent storage. Change todo functionalities => now it appears before the other todos instead of after and the textarea disappears when adding a todo.

--- src/pages/todo/AddTodo.tsx
+++ src/pages/todo/AddTodo.tsx
@@ -1,14 +1,36 @@
+import type { Todo } from '../../types/todo';
 import './AddTodo.css';
+import {useState} from 'react';
 
 interface AddTodoProps {
     isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-export function AddTodo({isOpen}: AddTodoProps) {
+export function AddTodo({ isOpen, setIsOpen, setTodos }: AddTodoProps) {
+
+const [content, setContent] = useState('');
+
+const handleAdd = () => {
+    const newTodo: Todo = {
+      id: crypto.randomUUID(),
+      completed: false,
+      content,
+      date: Date.now(),
+      difficulty: 1,
+    };
+    console.log('before',isOpen)
+    setIsOpen(v => !v)
+    console.log('after',isOpen)
+    setTodos(prev => [newTodo, ...prev]);
+    setContent('');
+  };
+
     return (
         <div className={`todo-input-group ${isOpen ? 'show' : ''}`}>
-            <textarea className="todo-input" />
-            <button className="todo-input-add">Add</button>
+            <textarea className="todo-input" value={content} onChange={e => setContent(e.target.value)} />
+            <button className="todo-input-add" onClick={handleAdd}  >Add</button>
         </div>
     )
 }
--- src/pages/todo/TodoList.tsx
+++ src/pages/todo/TodoList.tsx
@@ -1,5 +1,5 @@
 import './TodoList.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import type { Todo } from '../../types/todo';
 import { AddTodo } from './AddTodo';
 import rawTodoData from '../../data/TodoData.json'
@@ -7,12 +7,21 @@ import { TodoHeader } from './TodoHeader';
 import { TodoItem } from './TodoItem';
 
 export function TodoList() {
-    const [todos, setTodos] = useState<Todo[]>(rawTodoData as Todo[])
+    const [todos, setTodos] = useState<Todo[]>(() => {
+        const stored = localStorage.getItem('todos');
+        return stored ? (JSON.parse(stored) as Todo[]) : rawTodoData as Todo[]
+
+    })
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+    
     const [isOpen, setIsOpen] = useState(false);
     return (
         <section className="todo-list">
             <TodoHeader onToggle={() => setIsOpen(v => !v)} isOpen={isOpen} />
-            <AddTodo isOpen={isOpen} />
+            <AddTodo isOpen={isOpen} setIsOpen={setIsOpen} setTodos={setTodos} />
             <ul>
                 {todos.map((todo) => {
                     return <TodoItem key={todo.id} todo={todo} />
