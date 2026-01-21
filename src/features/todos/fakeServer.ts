b63d08507f5ca1dc477c99d993f1512527be9ce8
olegbucolo <olegbucolo97@gmail.com>
2026-01-21 23:04:22 +0100

    Refactor[fakeServer.ts]: removed static data and implemented api for getting and saving

--- src/features/todos/fakeServer.ts
+++ src/features/todos/fakeServer.ts
@@ -1,16 +1,29 @@
 import type { Todo } from '../../types/todo';
-import rawTodoData from '../../data/TodoData.json';
+// import rawTodoData from '../../data/TodoData.json';
 
-let serverTodos: Todo[] = [...rawTodoData];
+// let serverTodos: Todo[] = [...rawTodoData];
 
-const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+// const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+// export async function fetchTodos(): Promise<Todo[]> {
+//     await delay(500);
+//     return [...serverTodos];
+// }
+
+// export async function saveTodos(todos: Todo[]): Promise<void> {
+//     await delay(500);
+//     serverTodos = [...todos];
+// }
 
 export async function fetchTodos(): Promise<Todo[]> {
-    await delay(500);
-    return [...serverTodos];
+    const res = await fetch('http://localhost:8000/api/todos');
+    return res.json();
 }
 
 export async function saveTodos(todos: Todo[]): Promise<void> {
-    await delay(500);
-    serverTodos = [...todos];
+    await fetch('http://localhost:8000/api/todos', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(todos),
+    });
 }
